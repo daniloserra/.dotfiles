55bd8340a0b9d7cb9164d583bc826ef5e2266226
danilo <daniloserra@gmail.com>
2020-04-16 13:29:20 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh 
+
+apt install -y stow vim tmux zsh
+cd ~/teste/
+
+if [ -e .vimrc ]
+then
+	mv .vimrc .vimrc.old
+fi
+
+if [ -e .vim ]
+then
+	mv .vim .vim.old
+fi
+
+if [ -e .bashrc ]
+then
+	mv .bashrc .bashrc.old
+fi
+
+if [ -e .zshrc ]
+then
+	mv .zshrc .zshrc.old
+fi
+
+cd ~/.dotfiles
+stow vim bash zsh
